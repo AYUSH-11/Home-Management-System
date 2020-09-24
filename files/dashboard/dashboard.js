718d551797a1db16832d7c019e33f92f702bad3a
AYUSH-11 <65452816+AYUSH-11@users.noreply.github.com>
2020-09-25 00:14:38 +0530

    Add login process design

--- files/dashboard/dashboard.js
+++ files/dashboard/dashboard.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   var  length=5;
-  var name="hello";
+  var name="HELLO";
   var image="crop.jpg"
  
   for(var i=0;i<length;i++)
     {
-      $("#record").append(" <div class=\"flip-card\" style=\" float:left ;width:32%; padding:5px;height:325px\">\
+      $("#record").append(" <div class=\"flip-card\" style=\" float:left ;width:32%; padding:10px;height:325px\">\
       	<div class=\"flip-card-inner\">\
       			<div class=\"flip-card-front\">\
-      				<img src="+image+" alt=\"Avatar\" style=\"width:300px;height:300px;\">\
+      				<img src="+image+" alt=\"Avatar\" style=\"width:300px;height:260px;\">\
       			<div>\
-      			</div>"+ name+"</div>\
+      			</div><h1>"+ name+"</h1></div>\
       			<div class=\"flip-card-back\">\
       			<br><br>\
       			<button style=\"font-size: 25px;background-color: lightblue\">view</button>\
