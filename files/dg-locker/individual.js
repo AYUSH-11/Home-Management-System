be06dc8f2139b66c748adbc8ab383f092e9d0974
AYUSH-11 <65452816+AYUSH-11@users.noreply.github.com>
2020-09-26 23:43:42 +0530

    Connection

---
+++ files/dg-locker/individual.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var  length=6;
+  var name="HELLO";
+  var image="../../crop.jpg"
+  var image=["../../crop.jpg"];
+  var opendocument=["individualdocument.html"];
+  for(var i=0;i<length;i++)
+    {
+      $("#indrec").append("<form action="+opendocument[0]+">\
+        <input type='image' src="+image[0]+" alt='Submit' width=25% height='250px' style='float:left;padding:10px;' >\
+      </form>");	
+    }
+
+  });
---
+++ files/dg-locker/individualdocument.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  var  length=6;
+  var name="HELLO";
+  
+  var image=["../../crop.jpg"];
+  var documentname=['Credit Card']
+  var opendocument=["homedocument.html"];
+  for(var i=0;i<length;i++)
+    { 
+      $("#inddoc").append("<div class='col-xl-4 col-lg-6 col-md-12 col-sm-12 col-12' style='float:left;'>\
+                              <div class='product-thumbnail'>\
+                                  <div class='product-img-head'>\
+                                    <div class='product-img'>\
+                                                    <img src="+image[0]+"  class='img-fluid'>\
+                                    </div>\
+                                    <div class='ribbons'></div>\
+                                    <div class='ribbons-text'>New</div>\
+                                    <div class='product-content'>\
+                                        <div class='product-content-head'>\
+                                            <h3 class='product-title'>"+documentname[0]+"</h3>\
+                                        </div>\
+                                        <div class='product-btn'>\
+                                                <a href='#' class='btn btn-primary' style='width:45%;'>View</a>\
+                                                <a href='#' class='btn btn-primary' style='width:45%; float:right;'>Delete</a><br>\
+                                            <div style='text-align:center; padding-top: 5px;' >\
+                                                <a href='#' class='btn btn-primary'>Download</a>\
+                                            </div>\
+                                        </div>\
+                                    </div>\
+                                  </div>\
+                              </div>\
+                           </div>");
+      }
+
+  });
+
+
