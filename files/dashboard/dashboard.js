27377cba5fe4f2191388d3aaa23c825f8ae240f5
AYUSH-11 <65452816+AYUSH-11@users.noreply.github.com>
2020-09-27 11:36:00 +0530

    change in home document html file

--- files/dashboard/dashboard.js
+++ files/dashboard/dashboard.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
   var  length=5;
   var name="HELLO";
@@ -25,31 +24,3 @@ $(document).ready(function(){
     }
 
   });
-=======
-$(document).ready(function(){
-  var  length=5;
-  var name="HELLO";
-  var image="crop.jpg"
- 
-  for(var i=0;i<length;i++)
-    {
-      $("#record").append(" <div class=\"flip-card\" style=\" float:left ;width:32%; padding:10px;height:325px\">\
-      	<div class=\"flip-card-inner\">\
-      			<div class=\"flip-card-front\">\
-      				<img src="+image+" alt=\"Avatar\" style=\"width:300px;height:260px;\">\
-      			<div>\
-      			</div><h1>"+ name+"</h1></div>\
-      			<div class=\"flip-card-back\">\
-      			<br><br>\
-      			<button style=\"font-size: 25px;background-color: lightblue\">view</button>\
-      			<br><br><br><br>\
-      			<button style=\"font-size: 25px;background-color: lightblue\">update</button>\
-      			<br><br><br><br>\
-      			<button style=\"font-size: 25px;background-color: lightblue;\">delete</button>\
-      		</div>\
-      	</div>\
-      	</div>.");	
-    }
-
-  });
->>>>>>> e085a7e... Combining Bill and DG-locker
