8f4276a829b490bb5910b650ddb4888cc62af304
AYUSH-11 <65452816+AYUSH-11@users.noreply.github.com>
2020-09-27 16:53:19 +0530

    add add_user image

--- files/dashboard/dashboard.js
+++ files/dashboard/dashboard.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-  var  length=5;
-  var name="HELLO";
+  var  length=3;
+  var name=['HELLO','Ayush','Yash'];
   var image="crop.jpg"
- 
+  var images=['crop.jpg','crop.jpg','crop.jpg']
   for(var i=0;i<length;i++)
     {
       $("#record").append(" <div class=\"flip-card\" style=\" float:left ;width:32%; padding:10px;height:325px\">\
       	<div class=\"flip-card-inner\">\
       			<div class=\"flip-card-front\">\
-      				<img src="+image+" alt=\"Avatar\" style=\"width:300px;height:260px;\">\
+      				<img src="+images[i]+" alt=\"Avatar\" style=\"width:300px;height:260px;\">\
       			<div>\
-      			</div><h1>"+ name+"</h1></div>\
+      			</div><h1>"+ name[i]+"</h1></div>\
       			<div class=\"flip-card-back\">\
       			<br><br>\
       			<button style=\"font-size: 25px;background-color: lightblue\">view</button>\
